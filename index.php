e1064d1b6e4594f48a7e88b13a6c8269f8795fee
PaWuK11 <62262649+PaWuK11@users.noreply.github.com>
2022-06-29 13:52:09 +0300

    ended with html

--- index.php
+++ index.php
@@ -10,19 +10,17 @@
     <?php include ("header.php"); ?>    
     <hr>
     <div class="navigation">
-        <ul>
-            <a class="nav" href="#">ПРО СТУДІЮ</a>
-            <label for="">●</label>
-            <a class="nav" href="#">ПОРТФОЛІО</a>
-            <label for="">●</label>
-            <a class="nav" href="#">ЕТАПИ РОБОТИ</a>
-            <label for="">●</label>
-            <a class="nav" href="#">ВАРТІСТЬ</a>
-            <label for="">●</label>
-            <a class="nav" href="#">ЕКО-МАТЕРІАЛИ</a>
-            <label for="">●</label>
-            <a class="nav" href="#">КОНТАКТИ</a>
-        </ul>
+        <a class="nav" href="#">ПРО СТУДІЮ</a>
+        <label for="">●</label>
+        <a class="nav" href="#">ПОРТФОЛІО</a>
+        <label for="">●</label>
+        <a class="nav" href="#">ЕТАПИ РОБОТИ</a>
+        <label for="">●</label>
+        <a class="nav" href="#">ВАРТІСТЬ</a>
+        <label for="">●</label>
+        <a class="nav" href="#">ЕКО-МАТЕРІАЛИ</a>
+        <label for="">●</label>
+        <a class="nav" href="#">КОНТАКТИ</a>
     </div>
 
     <div class="video">
@@ -32,7 +30,44 @@
         </video>
     </div>
 
+    <div class="title-lvl1">
+        <label for="">●</label>
+        <h1>Нові проекти</h1>
+        <label for="">●</label>
+    </div>
 
+    <div class="post">
+        <div class="conteiner">
+            <img src="img/Bitmap.png" alt="">
+            <figcaption>Котедж Первый парковый</figcaption>
+            <img src="img/Bitmap (1).png" alt="">
+            <figcaption>Квартира ЖК Риверстоун</figcaption>
+            <img src="img/Bitmap (2).png" alt="">
+            <figcaption >Дом Грузевица-3</figcaption>
+        </div>
+        <div class="conteiner">
+            <img src="img/Bitmap (3).png" alt="">
+            <figcaption>Дом по улице Летняя</figcaption>
+            <img src="img/Bitmap (4).png" alt="">
+            <figcaption>Магазин Аристократ</figcaption>
+            <img src="img/Bitmap (5).png" alt="">
+            <figcaption>Магазин SEW TECH</figcaption>
+        </div>
+        <div class="conteiner">
+            <img src="img/Bitmap (6).png" alt="">
+            <figcaption>Апартаменты</figcaption>
+            <img src="img/Bitmap (7).png" alt="">
+            <figcaption>Бутик Mix</figcaption>
+            <img src="img/Bitmap (8).png" alt="">
+            <figcaption>Загородный дом</figcaption>
+        </div>
+    </div>
+
+    <div class="text-content">
+        <h2>Замовити дизайн інтер'єру в Хмельницькому для свого будинку!</h2>  
+        <label for="">Дім для кожного з нас – це місце, де ми відчуваємо комфорт і спокій, знаходимо душевний спокій, куди всі поспішаємо після важкого трудового дня, і де хочемо сховатися від смути міста. І, звичайно, у кожного є будинок мрії, який, напевно, був придуманий і візуалізований в дитинстві або, навпаки, є мрія, але чіткого уявлення про те, як він буде виглядати, немає. Команда We-ART з радістю допоможе вам виконати абсолютно будь-який дизайн інтер'єру Хмельницького.     За свою роботу команда студії дизайну інтер'єру в Хмельницькому We-ART накопичила достатньо знань і навичок, щоб задовольнити смаки будь-якого клієнта, а також виконала десятки проектів, в тому числі і для представників своєї сфери. Кожен клієнт унікальний, і кожен ставить свої пріоритети – для когось настав час, для кого-то ціна. Для команди We-ART пріоритетом є однакова якість завдань і побажань клієнта, тому наша компанія за весь час роботи була відкликана тільки з хорошого боку.</label>
+        <p><a href="#">Читати більше</a></p>
+    </div>
     <?php include ("footer.php"); ?>
 </body>
 </html>
